6ca969a453af48967bbf27c2b874b0e04a15a851
vks <gajanraj.mohanaraj@epitech.eu>
2023-06-09 17:00:10 +0200

    restestttt

--- process-form.php
+++ process-form.php
@@ -1,36 +1,27 @@
 <?php
-ob_start();
-if (isset($_POST['message'])) {
-    // Mise en place des entêtes de l'email
-    $boundary = md5(rand());
-    $entete = 'MIME-Version: 1.0' . "\r\n";
-    $entete .= 'From: "Nom" <contact@vick.fr>' . "\r\n";
-    $entete .= 'Reply-To: ' . $_POST['email'] . "\r\n";
-    $entete .= 'Content-Type: multipart/alternative; boundary=' . $boundary . "\r\n";
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+    $to = 'gajanraj.m@gmail.com';  // Adresse e-mail de destination
+    $subject = 'Nouveau message depuis le formulaire de contact';  // Objet du message
 
-    // Construction du message en HTML et en texte brut
-    $message_texte = strip_tags($_POST['message']);
-    $message_html = '<html><body>';
-    $message_html .= '<p>Message envoyé depuis la page Contact de vick.fr</p>';
-    $message_html .= '<p><b>vick.fr : </b>' . htmlspecialchars($_POST['name']) . '<br>';
-    $message_html .= '<b>Email : </b>' . $_POST['email'] . '<br><br>';
-    $message_html .= '<b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
-    $message_html .= '</body></html>';
+    // Mise en place de l'entête de l'e-mail
+    $headers = "From: " . $_POST['name'] . " <" . $_POST['email'] . ">\r\n";
+    $headers .= "Reply-To: " . $_POST['email'] . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-    // Mise en place du corps de l'email
-    $corps = '--' . $boundary . "\r\n";
-    $corps .= 'Content-Type: text/plain; charset=utf-8' . "\r\n";
-    $corps .= 'Content-Transfer-Encoding: 8bit' . "\r\n\r\n";
-    $corps .= $message_texte . "\r\n\r\n";
-    $corps .= '--' . $boundary . "\r\n";
-    $corps .= 'Content-Type: text/html; charset=utf-8' . "\r\n";
-    $corps .= 'Content-Transfer-Encoding: 8bit' . "\r\n\r\n";
-    $corps .= $message_html . "\r\n\r\n";
-    $corps .= '--' . $boundary . '--';
+    // Construction du corps de l'e-mail
+    $message = '<html><body>';
+    $message .= '<h2>Nouveau message depuis le formulaire de contact</h2>';
+    $message .= '<p><b>Nom :</b> ' . $_POST['name'] . '</p>';
+    $message .= '<p><b>Email :</b> ' . $_POST['email'] . '</p>';
+    $message .= '<p><b>Message :</b> ' . $_POST['message'] . '</p>';
+    $message .= '</body></html>';
 
-    // Envoi de l'email
-    $retour = mail('gajanraj.m@gmail.com', 'Envoi depuis mon site', $corps, $entete);
-
-    echo '<script>alert("Message envoyé avec succès !");</script>';
+    // Envoi de l'e-mail
+    if (mail($to, $subject, $message, $headers)) {
+        echo '<script>alert("Message envoyé avec succès !");</script>';
+    } else {
+        echo '<script>alert("Une erreur s\'est produite lors de l\'envoi du message.");</script>';
+    }
 }
 ?>
